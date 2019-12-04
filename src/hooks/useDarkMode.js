f080d32fd64da01d82793b509abc193edd27c8c3
hyetigran <tigran.asriyan@gmail.com>
2019-12-04 21:21:03 +0400

    complete use of hooks with toggle inside navbar component

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = (key, initialValue) => {
   const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
 
   useEffect(() => {
-    if (darkMode) {
-      return document.querySelector("body").classList.toggle("dark-mode");
-    }
-    return document.querySelector("body").classList.toggle("dark-mode");
+    darkMode
+      ? document.querySelector("body").classList.add("dark-mode")
+      : document.querySelector("body").classList.remove("dark-mode");
   }, [darkMode]);
 
-  return [darkMode, setDarkMode, storedValue, setValue];
+  return [darkMode, setDarkMode];
 };
